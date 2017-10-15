dda4550cde362411e7a850217a3f5f6663e8826d
Thomas Kuenneth <thomas@thomaskuenneth.com>
2017-10-15 13:13:56 +0200

    wip

--- Kapitel04/LeeresProjekt/app/src/main/java/com/thomaskuenneth/leeresprojekt/MainActivity.java
+++ Kapitel04/LeeresProjekt/app/src/main/java/com/thomaskuenneth/leeresprojekt/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linearlayout_demo);//activity_main
+        setContentView(R.layout.activity_main);
 
         // ab hier beliebigen Code einfügen...
     }
